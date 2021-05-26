5d964ffd01fe90fae9f2df27e9a73b65858596d7
wib <vitalij.boshko@gmail.com>
2021-05-26 23:31:03 +0300

    added menuEditor and menuBar

--- View.java
+++ View.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
+import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
 import java.awt.*;
@@ -15,6 +16,14 @@ public class View extends JFrame implements ActionListener {
     private JTextPane htmlTextPane = new JTextPane();
     //компонент для редактирования html в виде текста, он будет отображать код html (теги и их содержимое)
     private JEditorPane plainTextPane = new JEditorPane();
+
+    public View() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e){
+            ExceptionHandler.log(e);
+        }
+    }
 
     public Controller getController() {
         return controller;
@@ -38,12 +47,30 @@ public class View extends JFrame implements ActionListener {
 
     //инициализация менб редактора
     public void initMenuBar() {
+        JMenuBar jMenuBar = new JMenuBar();
+        MenuHelper.initFileMenu(this,jMenuBar);
+        MenuHelper.initEditMenu(this,jMenuBar);
+        MenuHelper.initStyleMenu(this,jMenuBar);
+        MenuHelper.initAlignMenu(this,jMenuBar);
+        MenuHelper.initColorMenu(this,jMenuBar);
+        MenuHelper.initFontMenu(this,jMenuBar);
+        MenuHelper.initHelpMenu(this,jMenuBar);
 
+        getContentPane().add(jMenuBar,BorderLayout.NORTH);
     }
 
     //инициализация панелей редактора
     public void initEditor() {
+        htmlTextPane.setContentType("text/html");
+        JScrollPane jScrollPane = new JScrollPane(htmlTextPane);
+        tabbedPane.addTab("HTML",jScrollPane);
+        JScrollPane jScrollPane1 = new JScrollPane(plainTextPane);
+        tabbedPane.addTab("Текст",jScrollPane1);
+        tabbedPane.setPreferredSize(new Dimension(100,100));
+        TabbedPaneChangeListener tabbedPaneChangeListener = new TabbedPaneChangeListener(this);
 
+        tabbedPane.addChangeListener(tabbedPaneChangeListener);
+        getContentPane().add(tabbedPane,BorderLayout.CENTER);
     }
 
     //инициализация графического интерфейса
